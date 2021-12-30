71a7d48aa87bdc491b24233ed5593e82af079b09
Daniel Emery <danielremery@gmail.com>
2021-12-30 23:12:47 +1030

    #7 Fix pre-check to refer to correct index script

--- src/pre-checks.ts
+++ src/pre-checks.ts
@@ -1,7 +1,6 @@
 import { promises as fs } from 'fs';
 import path from 'path';
-import { REQUIRED_INDEX_SCRIPT } from './constants';
-import { EnvironmentType, ProjectType, getIndexPath } from './environments';
+import { EnvironmentType, ProjectType, getIndexPath, getRequiredIndexScript } from './environments';
 
 export default async function performPreChecks(
   environmentType: EnvironmentType,
@@ -15,9 +14,10 @@ export default async function performPreChecks(
           getIndexPath(environmentType, projectType),
         );
         const file = await fs.readFile(indexPath, 'utf8');
-        if (file.indexOf(REQUIRED_INDEX_SCRIPT) < 0) {
+        const requiredIndexScript = getRequiredIndexScript(projectType);
+        if (file.indexOf(requiredIndexScript) < 0) {
           throw new Error(
-            `Could not find required script ${REQUIRED_INDEX_SCRIPT} in ${indexPath}`,
+            `Could not find required script ${requiredIndexScript} in ${indexPath}`,
           );
         }
       } catch (err: any) {
